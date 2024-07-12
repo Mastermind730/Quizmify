8924de72e49a7d33100ae4990aebeab7fca4faca
Mastermind730 <sourav.97032@gmail.com>
2024-07-12 20:32:34 +0530

    first commit

---
+++ src/lib/nextauth.ts
@@ -0,0 +1,13 @@
+import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+export const authOptions:NextAuthOptions={
+    session:{
+        strategy:'jwt',
+    },
+    callbacks:{
+        
+    }
+    secret:process.env.NEXTAUTH_SECRET,
+    adapter:PrismaAdapter(prisma),
+
+}
