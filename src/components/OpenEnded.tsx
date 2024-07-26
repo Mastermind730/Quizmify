8740df69c808e9656515d3fd5347b69151834e5d
Mastermind730 <sourav.97032@gmail.com>
2024-07-26 15:05:36 +0530

    completed with the openEnded questions page with checking answers and started the statistics page

--- src/components/OpenEnded.tsx
+++ src/components/OpenEnded.tsx
@@ -1,26 +1,27 @@
 "use client";
 import { Game, Question } from "@prisma/client";
 import React, { useCallback, useEffect, useMemo, useState } from "react";
-import { Button } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
-import { formatTimeDelta } from "@/lib/utils";
+import { cn, formatTimeDelta } from "@/lib/utils";
 import { differenceInSeconds } from "date-fns";
-import { ChevronRight, Loader2, Timer } from "lucide-react";
+import { BarChart, ChevronRight, Loader2, Timer } from "lucide-react";
 import { useToast } from "./ui/use-toast";
 import { checkAnswerSchema } from "@/schemas/form/quiz";
 import { useMutation } from "@tanstack/react-query";
 import { z } from "zod";
 import axios from "axios";
 import BlankAnswer from "./BlankAnswer";
+import Link from "next/link";
 
 type Props = {
   game: Game & { questions: Pick<Question, "id" | "question" | "answer">[]  };
 };
 
 const OpenEnded = ({ game }: Props) => {
-  const [questionIndex, setquestionIndex] = useState(0);
-  const [hasEnded, sethasEnded] = useState<boolean>(false);
-  const [blankAnswers,setBlankAnswers]=useState<String>("")
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [hasEnded, setHasEnded] = useState<boolean>(false);
+  const [blankAnswers, setBlankAnswers] = useState<String>("");
   const [now, setNow] = useState<Date>(new Date());
   const currentQuestion = useMemo(() => {
     return game.questions[questionIndex];
@@ -35,17 +36,20 @@ const OpenEnded = ({ game }: Props) => {
     return () => {
       clearInterval(interval);
     };
-  });
+  }, [hasEnded]);
+
   const { toast } = useToast();
 
-
-
-  
   const { mutate: checkAnswer, isPending: isChecking } = useMutation({
     mutationFn: async () => {
+      let filledAnswer = blankAnswers;
+      document.querySelectorAll("#user-blank-input").forEach(input => {
+        filledAnswer = filledAnswer.replace("_______", input.value);
+        input.value = "";
+      });
       const payload: z.infer<typeof checkAnswerSchema> = {
         questionId: currentQuestion.id,
-        userAnswer: "",
+        userAnswer: filledAnswer,
       };
       const response = await axios.post("/api/checkAnswer", payload);
       return response.data;
@@ -54,26 +58,21 @@ const OpenEnded = ({ game }: Props) => {
 
   const handleNext = useCallback(() => {
     if (isChecking) return;
-    console.log(blankAnswers);
-    return;
     checkAnswer(undefined, {
       onSuccess: ({ percentageSimilar }) => {
         toast({
-            title:`Your answer is ${percentageSimilar}% similar to the correct answer`,
-            description:"Answers are matched based on similarity comparisons"
-        })
+          title: `Your answer is ${percentageSimilar}% similar to the correct answer`,
+          description: "Answers are matched based on similarity comparisons",
+        });
 
+        setQuestionIndex(prev => prev + 1);
 
         if (questionIndex === game.questions.length - 1) {
-          sethasEnded(true);
-          return;
+          setHasEnded(true);
         }
-        setquestionIndex((prev) => prev + 1);
       },
     });
-  }, [checkAnswer,toast ,blankAnswers, isChecking, game.questions.length, questionIndex]);
-
-
+  }, [checkAnswer, toast, blankAnswers, isChecking, game.questions.length, questionIndex]);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -86,6 +85,20 @@ const OpenEnded = ({ game }: Props) => {
       document.removeEventListener("keydown", handleKeyDown);
     };
   }, [handleNext]);
+
+  if (hasEnded) {
+    return (
+      <div className="absolute flex flex-col justify-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="px-4 mt-2 font-semibold text-white bg-green-500 rounded-md whitespace-nowrap">
+          You completed in {formatTimeDelta(differenceInSeconds(now, game.timeStarted))}
+        </div>
+        <Link href={`/statistics/${game.id}`} className={cn(buttonVariants(), "mt-2")}>
+          View Statistics
+          <BarChart className="w-4 h-4 ml-2" />
+        </Link>
+      </div>
+    );
+  }
 
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-[80vw] max-w-4xl w-[90vw]">
@@ -100,13 +113,8 @@ const OpenEnded = ({ game }: Props) => {
           <div className="flex self-start mt-3 text-slate-400">
             <Timer className="mr-2" />
             {formatTimeDelta(differenceInSeconds(now, game.timeStarted))}
-            <span>00:00</span>
           </div>
         </div>
-        {/* <MCQCounter
-        correctAnswers={correctAnswers}
-        wrongAnswers={wrongAnswers}
-      /> */}
       </div>
       <Card className="w-full mt-4">
         <CardHeader className="flex flex-row items-center">
@@ -121,12 +129,11 @@ const OpenEnded = ({ game }: Props) => {
           </CardDescription>
         </CardHeader>
       </Card>
-
       <div className="flex flex-col items-center justify-center w-full mt-4">
-     <BlankAnswer answer={currentQuestion.answer} setBlankAnswer={setBlankAnswers}/>
+        <BlankAnswer answer={currentQuestion.answer} setBlankAnswer={setBlankAnswers} />
         <Button
           disabled={isChecking}
-          onClick={() => handleNext()}
+          onClick={handleNext}
           className="mt-2"
         >
           {isChecking && <Loader2 className="w-4 h-4 mr-2 animate-spin" />}
---
+++ src/components/statistics/ResultsCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Card, CardHeader } from '../ui/card'
+
+type Props = {}
+
+const ResultsCard = (props: Props) => {
+  return (
+    <Card className="md:cols-span-7">
+      <CardHeader>
+        
+      </CardHeader>
+    </Card>
+  )
+}
+
+export default ResultsCard
